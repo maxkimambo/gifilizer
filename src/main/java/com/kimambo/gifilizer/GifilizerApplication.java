8a46f33d410f06d4431944a583ab6bcfce158554
Max Kimambo <max@kimambo.de>
2018-04-02 03:25:44 -0700

    initial project setup

---
+++ src/main/java/com/kimambo/gifilizer/GifilizerApplication.java
@@ -0,0 +1,12 @@
+package com.kimambo.gifilizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifilizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GifilizerApplication.class, args);
+    }
+}
